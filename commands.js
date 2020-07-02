4f8759ac5e1f4865ba9b5d1b331f9e0938c15ed6
ccruz47 <christopherpaulcruz@gmail.com>
2020-07-02 10:28:07 -0700

    External js commands.

---
+++ commands.js
@@ -0,0 +1,11 @@
+client.on('message', (channel, tags, message, self) => {
+    if (self) return;
+    var commandData = parseCommand(message);
+    if (commandData.isValid) {
+        switch (commandData.command) {
+            case '!test':
+                client.say(channel, `@${tags.username}, test successful!`);
+                break;
+        }
+    }
+});
